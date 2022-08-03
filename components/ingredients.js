0450fff12040f65c8496aee4904f34aa2e27eb24
Noroleak Ing <ingnoroleak18@kit.edu.kh>
2022-08-03 17:53:54 +0700

    Add Ingredient Tabs and Finish Recipes Detail

---
+++ components/ingredients.js
@@ -0,0 +1,24 @@
+export default function Ingredients ()
+{
+  return (
+    <div className="card w-1/3 h-screen outline text-primary bg-base-200">
+      <div className="card-body p-5 text-neutral hover:underline-offset-2">
+        <h2 className="card-title justify-center px-3 underline">INGREDIENTS</h2>
+        
+        <li>2 tablespoons vegetable oil</li>
+        <li>1 thumb size fresh ginger, peeled and grated</li>
+        <li>1 apple such as Honeycrisp, peeled, cored, and grated</li>
+        <li>1 large onion, thinly sliced</li>
+        <li>5 cups water</li>
+        <li>1 large carrot, peeled and chopped</li>
+        <li>2 medium potatoes, peeled and chopped</li>
+        <li>1 1/2 cup green beans, ends trimmed and stringy part removed</li>
+        <li>1 Japanese or Chinese eggplant, chopped</li>
+        <li>8 cherry tomatoes</li>
+        <li>1 box Japanese curry roux mix (I’m using Java Curry)</li>
+        <li>Cooked Japanese short grain rice</li>       
+        
+      </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,55 @@
-import Navbar from '../components/navbar'
-import Footer from '../components/footer'
-import IngredientsList from '../components/ingredients-list.js'
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import IngredientsList from "../components/ingredients-list.js";
+import IngredientTabs from "../components/ingredient-tabs.js";
 
 export default function Home ()
 {
   return (
-    <div>
+    <div
+      className="flex flex-col justify-between"
+      style={ { minHeight: "100vh" } }
+    >
       <Navbar />
+      <div className="flex-1 p-8 h-full justify-center md:flex">
+        <div className="md:order-last md:ml-4">
+          <IngredientsList text="Find Recipes" />
+        </div>
 
-      <div className="m-10">
-        Welcome Home!
-        <button className="btn m-10">Hello daisyUI</button>
-        <IngredientsList />
+        <div className="h-4 md:hidden" />
+        <div className="md:grow">
+          {/* Search Bar */ }
+          <div className="form-control mb-4">
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Search…"
+                className="input input-bordered"
+              />
+              <button className="btn btn-square">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          {/* Ingredients */ }
+          <IngredientTabs />
+        </div>
       </div>
-
       <Footer />
     </div>
-  )
+  );
 }
