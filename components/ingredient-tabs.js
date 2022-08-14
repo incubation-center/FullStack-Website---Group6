ed31829fce1fac0f6c6b61a1021b4942837b9364
vatanak10 <koeurnsereyvatanak@gmail.com>
2022-08-14 14:26:01 +0700

    theme switch UI

--- components/ingredient-tabs.js
+++ components/ingredient-tabs.js
@@ -1,62 +1,58 @@
 import { useState } from "react";
 
-function IngredientTabs ()
-{
-  const [ activeTab, setActiveTab ] = useState( 1 );
+function IngredientTabs() {
+  const [activeTab, setActiveTab] = useState(1);
 
-  const handleSwitchTab = ( id ) =>
-  {
-    setActiveTab( id );
+  const handleSwitchTab = (id) => {
+    setActiveTab(id);
   };
 
   return (
     <div className="h-full flex flex-col overflow-x-scroll">
-      {/* Tab nav */ }
+      {/* Tab nav */}
       <ul className="flex text-center border-b border-gray-200">
-        { ingredients.map( ( ingredient ) =>
-        {
+        {ingredients.map((ingredient) => {
           return (
             <li
-              key={ ingredient.id }
-              className={ `flex-1 block p-4 text-sm font-medium rounded-t-lg capitalize cursor-pointer ${ activeTab === ingredient.id
+              key={ingredient.id}
+              className={`flex-1 block p-4 text-sm font-medium rounded-t-lg capitalize cursor-pointer ${
+                activeTab === ingredient.id
                   ? "relative bg-white border-t border-l border-r border-gray-200"
                   : "text-gray-500"
-                }` }
-              onClick={ () => handleSwitchTab( ingredient.id ) }
+              }`}
+              onClick={() => handleSwitchTab(ingredient.id)}
             >
-              { activeTab === ingredient.id && (
+              {activeTab === ingredient.id && (
                 <span className="absolute inset-x-0 w-full h-px bg-white -bottom-px"></span>
-              ) }
-              { ingredient.name }
+              )}
+              {ingredient.name}
             </li>
           );
-        } ) }
+        })}
       </ul>
 
-      {/* Tab content */ }
+      {/* Tab content */}
       <div>
-        { ingredients.map( ( ingredient ) =>
-        {
+        {ingredients.map((ingredient) => {
           return (
-            <div key={ ingredient.id } className="flex-1">
-              { ingredient.id === activeTab && (
+            <div key={ingredient.id} className="flex-1">
+              {ingredient.id === activeTab && (
                 <div className="form-control items-start flex border border-t-0 p-2 grow">
-                  { ingredient.items.map( ( item ) =>
-                  {
+                  {ingredient.items.map((item) => {
                     return (
-                      <label key={ item.id } className="label cursor-pointer">
+                      <label key={item.id} className="label cursor-pointer">
                         <input type="checkbox" className="checkbox" />
                         <span className="label-text ml-5 capitalize">
-                          { item.name }
+                          {item.name}
                         </span>
                       </label>
                     );
-                  } ) }
+                  })}
                 </div>
-              ) }
+              )}
             </div>
           );
-        } ) }
+        })}
       </div>
     </div>
   );
