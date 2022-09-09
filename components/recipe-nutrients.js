62c3b352143f6a811dde240227cb7864db2021a0
Noroleak Ing <ingnoroleak18@kit.edu.kh>
2022-09-09 14:23:10 +0700

    Edit recipe-nutrients

--- components/recipe-nutrients.js
+++ components/recipe-nutrients.js
@@ -1,26 +1,26 @@
-function RecipeNutrients (props)
+function RecipeNutrients ( props )
 {
-  const cookTime = props.item.durationSecond /60
+  const cookTime = props.item.durationSecond / 60
   const nutrients = props.item
   const nutrientsMaxValue = {
-      "durationSecond": 9900,
-      "calories": 1701.81,
-      "protein": 68.34,
-      "carb": 122.85,
-      "fiber": 25.99,
-      "fat": 104.12,
-      "sugar": 48.91,
-      "cholesterol": 835.51
+    "durationSecond": 9900,
+    "calories": 1701.81,
+    "protein": 68.34,
+    "carb": 122.85,
+    "fiber": 25.99,
+    "fat": 104.12,
+    "sugar": 48.91,
+    "cholesterol": 835.51
   }
 
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-8 gap-5 lg:gap-10 xl:gap-20">
-      {/* Cook Time */}
+      {/* Cook Time */ }
       <label className="swap swap-flip text-9xl">
-        {/* This hidden checkbox controls the state */}
+        {/* This hidden checkbox controls the state */ }
         <input type="checkbox" style={ { opacity: 0 } } />
-        {/* Avatar */}
+        {/* Avatar */ }
         <div className="avatar placeholder swap-off">
           <div className="w-32 rounded-full ring-2 ring-secondary hover:ring-4">
             <div className="flex flex-col items-center space-y-3">
@@ -32,17 +32,17 @@ function RecipeNutrients (props)
             </div>
           </div>
         </div>
-        {/* Radial Progress */}
-        <div className="radial-progress text-xl font-bold text-secondary swap-on" style={ { "--value": (cookTime/(nutrientsMaxValue.durationSecond/60))*100 , "--size": "8rem", "--thickness": "4px" } }>
-          {cookTime} mins
+        {/* Radial Progress */ }
+        <div className="radial-progress text-xl font-bold text-secondary swap-on" style={ { "--value": ( cookTime / ( nutrientsMaxValue.durationSecond / 60 ) ) * 100, "--size": "8rem", "--thickness": "4px" } }>
+          { cookTime } mins
         </div>
       </label>
 
-      {/* Calories */}
+      {/* Calories */ }
       <label className="swap swap-flip text-9xl">
-        {/* This hidden checkbox controls the state */}
+        {/* This hidden checkbox controls the state */ }
         <input type="checkbox" style={ { opacity: 0 } } />
-        {/* Avatar */}
+        {/* Avatar */ }
         <div className="avatar placeholder swap-off">
           <div className="w-32 rounded-full ring-2 ring-error hover:ring-4">
             <div className="flex flex-col items-center space-y-2">
@@ -53,9 +53,9 @@ function RecipeNutrients (props)
             </div>
           </div>
         </div>
-        {/* Radial Progress */}
-        <div className="radial-progress text-xl font-bold text-error swap-on" style={ { "--value": (nutrients.calories/nutrientsMaxValue.calories)*100, "--size": "8rem", "--thickness": "4px" } }>
-          {nutrients.calories}
+        {/* Radial Progress */ }
+        <div className="radial-progress text-xl font-bold text-error swap-on" style={ { "--value": ( nutrients.calories / nutrientsMaxValue.calories ) * 100, "--size": "8rem", "--thickness": "4px" } }>
+          { nutrients.calories }
         </div>
       </label>
 
@@ -75,8 +75,8 @@ function RecipeNutrients (props)
           </div>
         </div>
         {/* Radial Progress */ }
-        <div className="radial-progress text-xl font-bold text-info swap-on" style={ { "--value": (nutrients.protein/nutrientsMaxValue.protein)*100, "--size": "8rem", "--thickness": "4px" } }>
-          {nutrients.protein}
+        <div className="radial-progress text-xl font-bold text-info swap-on" style={ { "--value": ( nutrients.protein / nutrientsMaxValue.protein ) * 100, "--size": "8rem", "--thickness": "4px" } }>
+          { nutrients.protein }
         </div>
       </label>
 
@@ -87,9 +87,9 @@ function RecipeNutrients (props)
         {/* Avatar */ }
         <div className="avatar placeholder swap-off">
           <div className="w-32 rounded-full ring-2 ring-orange-400 hover:ring-4">
-            <div className="flex flex-col items-center space-y-3">            
+            <div className="flex flex-col items-center space-y-3">
               <svg className="color: rgb(251, 146, 60); w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                {/* Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. */}
+                {/* Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. */ }
                 <path d="M256 112V128C256 136.8 248.8 144 240 144C195.8 144 160 108.2 160 64V48C160 39.16 167.2 32 176 32C220.2 32 256 67.82 256 112zM104 64C117.3 64 128 74.75 128 88C128 101.3 117.3 112 104 112H56C42.75 112 32 101.3 32 88C32 74.75 42.75 64 56 64H104zM136 136C149.3 136 160 146.7 160 160C160 173.3 149.3 184 136 184H24C10.75 184 0 173.3 0 160C0 146.7 10.75 136 24 136H136zM32 232C32 218.7 42.75 208 56 208H104C117.3 208 128 218.7 128 232C128 245.3 117.3 256 104 256H56C42.75 256 32 245.3 32 232zM272 48C272 39.16 279.2 32 288 32C332.2 32 368 67.82 368 112V128C368 136.8 360.8 144 352 144C307.8 144 272 108.2 272 64V48zM480 112V128C480 136.8 472.8 144 464 144C419.8 144 384 108.2 384 64V48C384 39.16 391.2 32 400 32C444.2 32 480 67.82 480 112zM480 208C480 252.2 444.2 288 400 288C391.2 288 384 280.8 384 272V256C384 211.8 419.8 176 464 176C472.8 176 480 183.2 480 192V208zM352 176C360.8 176 368 183.2 368 192V208C368 252.2 332.2 288 288 288C279.2 288 272 280.8 272 272V256C272 211.8 307.8 176 352 176zM256 208C256 252.2 220.2 288 176 288C167.2 288 160 280.8 160 272V256C160 211.8 195.8 176 240 176C248.8 176 256 183.2 256 192V208zM0 352C0 334.3 14.33 320 32 320H480C497.7 320 512 334.3 512 352C512 411.7 471.1 461.9 415.8 476C415.9 477.3 416 478.7 416 480C416 497.7 401.7 512 384 512H128C110.3 512 96 497.7 96 480C96 478.7 96.08 477.3 96.24 476C40.91 461.9 0 411.7 0 352V352z" fill="#fb923c" />
               </svg>
               <p className="text-xl font-bold text-orange-400">Carb</p>
@@ -97,16 +97,16 @@ function RecipeNutrients (props)
           </div>
         </div>
         {/* Radial Progress */ }
-        <div className="radial-progress text-xl font-bold text-orange-400 swap-on" style={ { "--value": (nutrients.carb/nutrientsMaxValue.carb)*100, "--size": "8rem", "--thickness": "4px" } }>
-          {nutrients.carb}
+        <div className="radial-progress text-xl font-bold text-orange-400 swap-on" style={ { "--value": ( nutrients.carb / nutrientsMaxValue.carb ) * 100, "--size": "8rem", "--thickness": "4px" } }>
+          { nutrients.carb }
         </div>
       </label>
 
-      {/* Fiber */}
+      {/* Fiber */ }
       <label className="swap swap-flip text-9xl">
-        {/* This hidden checkbox controls the state */}
+        {/* This hidden checkbox controls the state */ }
         <input type="checkbox" style={ { opacity: 0 } } />
-        {/* Avatar */}
+        {/* Avatar */ }
         <div className="avatar placeholder swap-off">
           <div className="w-32 rounded-full ring-2 ring-success hover:ring-4">
             <div className="flex flex-col items-center space-y-3">
@@ -118,9 +118,9 @@ function RecipeNutrients (props)
             </div>
           </div>
         </div>
-        {/* Radial Progress */}
-        <div className="radial-progress text-xl font-bold text-success swap-on" style={ { "--value": (nutrients.fiber/nutrientsMaxValue.fiber)*100, "--size": "8rem", "--thickness": "4px" } }>
-          {nutrients.fiber}
+        {/* Radial Progress */ }
+        <div className="radial-progress text-xl font-bold text-success swap-on" style={ { "--value": ( nutrients.fiber / nutrientsMaxValue.fiber ) * 100, "--size": "8rem", "--thickness": "4px" } }>
+          { nutrients.fiber }
         </div>
       </label>
 
@@ -133,7 +133,7 @@ function RecipeNutrients (props)
           <div className="w-32 rounded-full ring-2 ring-yellow-400 hover:ring-4">
             <div className="flex flex-col items-center space-y-2">
               <svg className="color: rgb(250, 204, 21); w-12 h-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                {/* Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. */}
+                {/* Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. */ }
                 <path d="M215.3 143.4C243.5 95.07 274.2 49.29 294.9 19.3C307.2 1.585 332.8 1.585 345.1 19.3C393.7 89.43 496 245.9 496 319.1C496 333.7 494.4 347.1 491.5 359.9L630.8 469.1C641.2 477.3 643.1 492.4 634.9 502.8C626.7 513.2 611.6 515.1 601.2 506.9L9.196 42.89C-1.236 34.71-3.065 19.63 5.112 9.196C13.29-1.236 28.37-3.065 38.81 5.112L215.3 143.4zM143.1 319.1C143.1 296.5 154.3 264.6 169.1 229.9L443.5 445.4C411.7 476.7 368.1 496 319.1 496C222.8 496 143.1 417.2 143.1 319.1V319.1zM239.1 319.1C239.1 311.2 232.8 303.1 223.1 303.1C215.2 303.1 207.1 311.2 207.1 319.1C207.1 381.9 258.1 432 319.1 432C328.8 432 336 424.8 336 416C336 407.2 328.8 400 319.1 400C275.8 400 239.1 364.2 239.1 319.1V319.1z" fill="#facc15" />
               </svg>
               <p className="text-xl font-bold text-yellow-400">Fat</p>
@@ -141,16 +141,16 @@ function RecipeNutrients (props)
           </div>
         </div>
         {/* Radial Progress */ }
-        <div className="radial-progress text-xl font-bold text-yellow-400 swap-on" style={ { "--value": (nutrients.fat/nutrientsMaxValue.fat)*100, "--size": "8rem", "--thickness": "4px" } }>
-          {nutrients.fat}
+        <div className="radial-progress text-xl font-bold text-yellow-400 swap-on" style={ { "--value": ( nutrients.fat / nutrientsMaxValue.fat ) * 100, "--size": "8rem", "--thickness": "4px" } }>
+          { nutrients.fat }
         </div>
       </label>
 
-      {/* Sugar */}
+      {/* Sugar */ }
       <label className="swap swap-flip text-9xl">
-        {/* This hidden checkbox controls the state */}
+        {/* This hidden checkbox controls the state */ }
         <input type="checkbox" style={ { opacity: 0 } } />
-        {/* Avatar */}
+        {/* Avatar */ }
         <div className="avatar placeholder swap-off">
           <div className="w-32 rounded-full ring-2 ring-neutral/50 dark:ring-accent/50 hover:ring-4">
             <div className="flex flex-col items-center space-y-2">
@@ -161,31 +161,31 @@ function RecipeNutrients (props)
             </div>
           </div>
         </div>
-        {/* Radial Progress */}
-        <div className="radial-progress text-xl font-bold text-neutral/50 dark:text-accent/50 swap-on" style={ { "--value": (nutrients.sugar/nutrientsMaxValue.sugar)*100, "--size": "8rem", "--thickness": "4px" } }>
-          {nutrients.sugar}
+        {/* Radial Progress */ }
+        <div className="radial-progress text-xl font-bold text-neutral/50 dark:text-accent/50 swap-on" style={ { "--value": ( nutrients.sugar / nutrientsMaxValue.sugar ) * 100, "--size": "8rem", "--thickness": "4px" } }>
+          { nutrients.sugar }
         </div>
       </label>
 
-      {/* Cholesterol */}
+      {/* Cholesterol */ }
       <label className="swap swap-flip text-9xl">
-        {/* This hidden checkbox controls the state */}
+        {/* This hidden checkbox controls the state */ }
         <input type="checkbox" style={ { opacity: 0 } } />
-        {/* Avatar */}
+        {/* Avatar */ }
         <div className="avatar placeholder swap-off">
           <div className="w-32 rounded-full ring-2 ring-warning hover:ring-4">
             <div className="flex flex-col items-center space-y-3">
               <svg className="color: rgb(251, 189, 35); w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                {/* Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. */}
+                {/* Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. */ }
                 <path d="M352.4 243.8l-49.83 99.5c-6.009 12-23.41 11.62-28.92-.625L216.7 216.3l-30.05 71.75L88.55 288l176.4 182.2c12.66 13.07 33.36 13.07 46.03 0l176.4-182.2l-112.1 .0052L352.4 243.8zM495.2 62.86c-54.36-46.98-137.5-38.5-187.5 13.06L288 96.25L268.3 75.92C218.3 24.36 135.2 15.88 80.81 62.86C23.37 112.5 16.84 197.6 60.18 256h105l35.93-86.25c5.508-12.88 23.66-13.12 29.54-.375l58.21 129.4l49.07-98c5.884-11.75 22.78-11.75 28.67 0l27.67 55.25h121.5C559.2 197.6 552.6 112.5 495.2 62.86z" fill="#fbbd23" />
               </svg>
               <p className="text-xl font-bold text-warning">Cholesterol</p>
             </div>
           </div>
         </div>
-        {/* Radial Progress */}
-        <div className="radial-progress text-xl font-bold text-warning swap-on" style={ { "--value": (nutrients.cholesterol/nutrientsMaxValue.cholesterol)*100, "--size": "8rem", "--thickness": "4px" } }>
-          {nutrients.cholesterol}
+        {/* Radial Progress */ }
+        <div className="radial-progress text-xl font-bold text-warning swap-on" style={ { "--value": ( nutrients.cholesterol / nutrientsMaxValue.cholesterol ) * 100, "--size": "8rem", "--thickness": "4px" } }>
+          { nutrients.cholesterol }
         </div>
       </label>
     </div>
