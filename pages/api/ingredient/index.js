8904db5bf26c6a874ab164a3579e48a7c316699d
ThyMakra <makra279@gmail.com>
2022-09-15 10:41:40 +0700

    feat(api/ingredient): api to get paginated data

--- pages/api/ingredient/index.js
+++ pages/api/ingredient/index.js
@@ -1,13 +1,31 @@
+import { DEFAULT_PAGE_LIMIT } from '../../../lib/constant';
+import { getPaginationResult } from '../../../lib/helpers';
 import { prisma } from '../../../lib/prisma';
 
 
 export default async function handle(req, res) {
-    if (req.method === 'GET') {
-        const params = req.query;
+  if (req.method === 'POST') {
+    const { filter, sort, relation, page = 1, limit = DEFAULT_PAGE_LIMIT } = req.body;
 
-        const ingredient = await prisma.ingredient.findFirst({
-            where: params,
-        });
-        res.json(ingredient);
-    }
+    const ingredientCount = await prisma.ingredient.count({
+        where: filter,
+    });
+    const ingredients = await prisma.ingredient.findMany({
+        where: filter,
+        orderBy: sort,
+        include: relation,
+        skip: limit * (page - 1),
+        take: limit,
+    });
+
+    res.status(200).json({
+      data: ingredients,
+      pagination: getPaginationResult(
+        ingredientCount,
+        limit,
+        ingredients.length,
+        page
+      )
+    });
+  }
 }
